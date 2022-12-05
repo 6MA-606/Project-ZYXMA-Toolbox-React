0efd24655857f0f929db818fbc05d450a9bbb3e9
6MA-606 <85861104+6MA-606@users.noreply.github.com>
2022-12-05 14:33:36 +0700

    2.5.3.6 Add Framer Motion Shortcut

--- src/tools/playground/main.js
+++ src/tools/playground/main.js
@@ -52,7 +52,7 @@ function Playground() {
                 <Shortcut thumbnail='./img/icon/canva.png' bgimage="linear-gradient(to right bottom, #01c2cc, #6722c7)" href="https://www.canva.com" icon />
                 <Shortcut thumbnail='./img/icon/audo.png' bgcolor="#222" href="https://audo.ai" icon tooltip="Audo"/>
                 <Shortcut thumbnail='./img/icon/colorsupply.png' bgcolor="#f8f8ff" href="https://colorsupplyyy.com/app" icon tooltip="Color Supply"/>
-                <Shortcut thumbnail='./img/icon/bootstrap5_icon.png' bgimage="url('./img/icon/bg/bootstrap5_icon_bg.jpg')" href="https://icons.getbootstrap.com" icon tooltip="Bootstrap5 Icon"/>
+                <Shortcut thumbnail='./img/icon/framermotion.png' bgcolor="#202020" href="https://www.framer.com/motion/" icon tooltip="Framer Motion"/>
             </ShortcutGroup>
             <ShortcutGroup type="icon">
                 <Shortcut thumbnail='./img/icon/colorhunt.png' bgcolor="#f8f8ff" href="https://colorhunt.co" icon tooltip="Color Hunt"/>
